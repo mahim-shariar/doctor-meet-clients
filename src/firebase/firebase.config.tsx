d8a9169a5bdf9860eab28f5e044c023d19780272
Mahadi Hasan <mhasancy@gmail.com>
2022-04-29 11:30:36 +0600

    added all files

---
+++ src/firebase/firebase.config.tsx
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCSq4MfEqH1VDvHBV6ilrS1ZENrn22lEHA",
+  authDomain: "doctor-meet-a0213.firebaseapp.com",
+  projectId: "doctor-meet-a0213",
+  storageBucket: "doctor-meet-a0213.appspot.com",
+  messagingSenderId: "648178460074",
+  appId: "1:648178460074:web:2ee3dce0056437d31df7d3",
+};
+export default firebaseConfig;
