eb3c7fccb87483ebf112425e767c6a7ec18f2b85
Al Amin <alaminhossain2000and@gmail.com>
2022-04-29 21:47:39 +0600

    popular test design done

---
+++ src/components/DiagnosticCenterComponents/PopularTestProcedure/PopularTestProcedure.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import  './PopularTestProcedure.css';
+
+
+const PopularTestProcedure = ({testProcedure}) => {
+//    console.log(testProcedure)
+  const intPrice=testProcedure.price;
+  const intDiscount=testProcedure.discount;
+    const floatDiscount=parseFloat(intDiscount).toFixed(2);
+    
+    const dd=floatDiscount/100.00;
+    // console.log(intPrice,dd);
+const floatPrice=intPrice-(intPrice*dd);
+  const {title,content,price,discount}=testProcedure;
+
+
+    return (
+        <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>{title}</Card.Title>
+    <Card.Subtitle className="mb-4 text-muted">{content}</Card.Subtitle>
+    
+     
+        <div className='d-flex  mt-5'>
+        <p className='text-decoration-line-through undiscounted-price'>${price}</p>
+      <p className='mx-2 discounted-price'>${floatPrice}</p>
+      <div className="discount-amount-holder">
+      <p>{discount} % Off</p>
+
+      </div>
+        </div>
+        
+        
+    
+    
+    {/* <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link> */}
+  </Card.Body>
+</Card>
+    );
+};
+
+export default PopularTestProcedure;
--- src/page/DiagnosticCenter/DiagnosticCenter.tsx
+++ src/page/DiagnosticCenter/DiagnosticCenter.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import CovidAwarnessBar from '../../components/DiagnosticCenterComponents/CovidAwarnessBar/CovidAwarnessBar';
 import DiagnosticCenterBanner from '../../components/DiagnosticCenterComponents/DiagnosticCenterBanner/DiagnosticCenterBanner';
+import PopularTestProcedures from '../../components/DiagnosticCenterComponents/PopularTestProcuders/PopularTestProcedures';
 
 
 
@@ -10,7 +12,8 @@ const DiagnosticCenter = () => {
         <div>
             <h1>Hello From Diagnostic Center</h1>
             <DiagnosticCenterBanner></DiagnosticCenterBanner>
-            
+            <CovidAwarnessBar></CovidAwarnessBar>
+            <PopularTestProcedures></PopularTestProcedures>
         </div>
     );
 };
