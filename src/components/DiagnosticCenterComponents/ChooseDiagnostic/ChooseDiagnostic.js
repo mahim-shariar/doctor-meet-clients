6cff2932662b02df721912e7a14abb6085e23d39
Al Amin <alaminhossain2000and@gmail.com>
2022-05-01 00:18:21 +0600

    diagnostic center is ready

--- src/components/DiagnosticCenterComponents/ChooseDiagnostic/ChooseDiagnostic.js
+++ src/components/DiagnosticCenterComponents/ChooseDiagnostic/ChooseDiagnostic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Col } from 'react-bootstrap';
+import {  Card, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './ChooseDiagnostic.css';
 const ChooseDiagnostic = ({section}) => {
