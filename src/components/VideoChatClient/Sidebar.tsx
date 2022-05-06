499db8a52c30ae6f8bbf8935e0f7dab24b95f179
Al Amin <alaminhossain2000and@gmail.com>
2022-05-06 18:43:54 +0600

    sidebar design done

--- src/components/VideoChatClient/Sidebar.tsx
+++ src/components/VideoChatClient/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { SocketContext } from "../../context/Context";
-
+import './Sidebar.css';
 const Sidebar = ({ children }: any) => {
   const { me, callAccepted, setName, callEnded, leaveCall, callUser } =
     useContext(SocketContext);
@@ -12,47 +12,53 @@ const Sidebar = ({ children }: any) => {
   };
   console.log(me);
   return (
-    <div>
+    <div className="video-chat-sidebar">
+       <CopyToClipboard text={me}>
+                <button className="btn-copy-id" onClick={(e)=>e.preventDefault()} title="Click to Copy">ID</button>
+              </CopyToClipboard>
       <form noValidate autoComplete="off">
         <div>
           <div style={{ width: "600px" }}>
-            <h1 className="">Account Info</h1>
+            <h1 className="sidebar-title text-center mb-5">Account Info</h1>
             <div className="input-group input-group-sm mb-3">
               <input
                 type="text"
-                className="form-control"
+                // className="form-control"
+                className="sidebar-form"
                 aria-label="Sizing example input"
                 aria-describedby="inputGroup-sizing-sm"
                 onChange={(e) => setName(e.target.value)}
+                placeholder="Enter Your Name"
               />
-              <CopyToClipboard text={me}>
-                <button className="btn btn-warning">Copy Your ID</button>
-              </CopyToClipboard>
+              
+         
+             
             </div>
           </div>
           <div>
-            <h1>Make a call</h1>
+           
             <div
               className="input-group input-group-sm mb-3"
               style={{ width: "600px" }}
             >
               <input
                 type="text"
-                className="form-control"
+                className="sidebar-form"
                 aria-label="Sizing example input"
                 aria-describedby="inputGroup-sizing-sm"
                 onChange={(e) => setIdToCall(e.target.value)}
-                placeholder="ID to call"
+                placeholder="Enter ID to call"
                 value={idToCall}
+                
               />
               {callAccepted && !callEnded ? (
                 <button className="btn btn-warning" onClick={leaveCall}>
                   Hang Up
                 </button>
               ) : (
-                <button className="btn btn-danger" onClick={callAUser}>
-                  Call
-                </button>
+                
+                  <img src="https://cdn-icons.flaticon.com/png/128/3059/premium/3059606.png?token=exp=1651840673~hmac=229c3147a5b14466a711b596bea30808" className="call-icon" alt="" onClick={callAUser} title="Call Now"/>
+                
               )}
             </div>
           </div>
