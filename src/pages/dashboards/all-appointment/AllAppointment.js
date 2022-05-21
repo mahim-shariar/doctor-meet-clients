58d486b7235c43ae56ad3db87f63b84abb03b4a6
Al Amin <alaminhossain2000and@gmail.com>
2022-05-21 17:08:57 +0600

    more info all appointment

--- src/pages/dashboards/all-appointment/AllAppointment.js
+++ src/pages/dashboards/all-appointment/AllAppointment.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import '../../../components/appointment/AppointmentStyle.css';
-
+function MyVerticallyCenteredModal(props) {
+ 
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Id : {props.appiontment?._id}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        
+        <p>
+          Patient Name : {props.appointment.patientName}
+        </p>
+        <p>
+          Patient Email : {props.appointment.patientEmail}
+        </p>
+        <p>
+          Contact Info : {props.appointment?.mobileNumber}
+        </p>
+        <p>
+          Gender : {props.appointment.gender}
+        </p>
+        <p>
+          Age : {props.appointment.age}
+        </p>
+        <p>
+          Blood Group : {props.appointment.bloodGroup}
+        </p>
+        <p>
+          Date : {props.appointment?.date}
+        </p>
+        <p>
+          Timeslot : {props.appointment.doctorInfo.timeSlot1||props.appointment.doctorInfo.timeSlot2||props.appointment.doctorInfo.timeSlot3||props.appointment.doctorInfo.timeSlot}
+        </p>
+        <p>
+          Doctor Name : {props.appointment.doctorInfo.name}
+        </p>
+        <p>
+          Doctor Email : {props.appointment.doctorInfo.email}
+        </p>
+        <p>
+          Doctor Phone : {props.appointment.doctorInfo.phone}
+        </p>
+        <p>
+          Doctor Department : {props.appointment.doctorInfo.specialist}
+        </p>
+        <p>
+          Doctor Visit : {props.appointment.doctorInfo.visit}
+        </p>
+        <p>
+          Payment Status : {props.appointment.status}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 const AllAppointment= ({appointment}) => {
+  const [modalShow, setModalShow] = React.useState(false);
+  
     const deleteAnAppointment=()=>{
       const confirm=window.confirm("Are you sure want to delete the data?");
       if(confirm){
@@ -13,7 +80,7 @@ const AllAppointment= ({appointment}) => {
         .then(data=>{
           if(data.deletedCount){
             alert("Data is Deleted Successfully");
-            
+
           }
         })
       }
@@ -28,7 +95,19 @@ const AllAppointment= ({appointment}) => {
       <td className="appointment-table-row-data">{appointment?.date}</td>
       <td className="appointment-table-row-data">{appointment?.status}</td>
       
-      <td className="appointment-table-row-data"><i className="fas fa-info"></i></td>
+      <td className="appointment-table-row-data">
+        
+        <>
+
+      <i className="fas fa-info" onClick={() => setModalShow(true)}></i>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        appointment={appointment}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+        </td>
       <td className="appointment-table-row-data">
         <i className="fas fa-trash" onClick={deleteAnAppointment}></i>
       </td>
